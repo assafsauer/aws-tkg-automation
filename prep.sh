25d2dbdfe907e1d903027cfa6f81d2068a0b3480
assafsauer <as.sauer@gmail.com>
2021-12-17 08:23:05 +0100

    Update prep.sh

--- prep.sh
+++ prep.sh
@@ -3,14 +3,77 @@
 
 sleep 30
 
- export AWS_ACCESS_KEY_ID=ASIAxxxxx
- export AWS_SECRET_ACCESS_KEY=7lqB4jx47kEXkxxxxx
- export AWS_SESSION_TOKEN=IQoJb3JpZ2luX2VjELb//////////wEaCXVzLxxxxxxx
+###### aws temporary access #######
+
+export AWS_ACCESS_KEY_ID=ASIAxxxxx
+export AWS_SECRET_ACCESS_KEY=7lqB4jx47kEXkxxxxx
+export AWS_SESSION_TOKEN=IQoJb3JpZ2luX2VjELbxxxLxxxxxxx
+export AWS_REGION=eu-west-1
  
- export AWS_REGION=eu-west-1
- 
- 
- 
+
+########  tkg mgmt configuration ########
+
+cat > mgmt.yaml << EOF
+AWS_AMI_ID: ami-0f210a57e0be8c9ef
+AWS_NODE_AZ: eu-west-1a
+AWS_NODE_AZ_1: ""
+AWS_NODE_AZ_2: ""
+AWS_PRIVATE_NODE_CIDR: 10.0.16.0/20
+AWS_PRIVATE_NODE_CIDR_1: ""
+AWS_PRIVATE_NODE_CIDR_2: ""
+AWS_PRIVATE_SUBNET_ID: ""
+AWS_PRIVATE_SUBNET_ID_1: ""
+AWS_PRIVATE_SUBNET_ID_2: ""
+AWS_PUBLIC_NODE_CIDR: 10.0.0.0/20
+AWS_PUBLIC_NODE_CIDR_1: ""
+AWS_PUBLIC_NODE_CIDR_2: ""
+AWS_PUBLIC_SUBNET_ID: ""
+AWS_PUBLIC_SUBNET_ID_1: ""
+AWS_PUBLIC_SUBNET_ID_2: ""
+AWS_REGION: eu-west-1
+AWS_SSH_KEY_NAME: sauer-key
+AWS_VPC_CIDR: 10.0.0.0/16
+AWS_VPC_ID: ""
+BASTION_HOST_ENABLED: "false"
+CLUSTER_CIDR: 100.96.0.0/11
+CLUSTER_NAME: mgmt
+CLUSTER_PLAN: dev
+CONTROL_PLANE_MACHINE_TYPE: t2.large
+ENABLE_AUDIT_LOGGING: ""
+ENABLE_CEIP_PARTICIPATION: "false"
+ENABLE_MHC: "true"
+IDENTITY_MANAGEMENT_TYPE: none
+INFRASTRUCTURE_PROVIDER: aws
+LDAP_BIND_DN: ""
+LDAP_BIND_PASSWORD: ""
+LDAP_GROUP_SEARCH_BASE_DN: ""
+LDAP_GROUP_SEARCH_FILTER: ""
+LDAP_GROUP_SEARCH_GROUP_ATTRIBUTE: ""
+LDAP_GROUP_SEARCH_NAME_ATTRIBUTE: cn
+LDAP_GROUP_SEARCH_USER_ATTRIBUTE: DN
+LDAP_HOST: ""
+LDAP_ROOT_CA_DATA_B64: ""
+LDAP_USER_SEARCH_BASE_DN: ""
+LDAP_USER_SEARCH_FILTER: ""
+LDAP_USER_SEARCH_NAME_ATTRIBUTE: ""
+LDAP_USER_SEARCH_USERNAME: userPrincipalName
+NODE_MACHINE_TYPE: t2.xlarge
+OIDC_IDENTITY_PROVIDER_CLIENT_ID: ""
+OIDC_IDENTITY_PROVIDER_CLIENT_SECRET: ""
+OIDC_IDENTITY_PROVIDER_GROUPS_CLAIM: ""
+OIDC_IDENTITY_PROVIDER_ISSUER_URL: ""
+OIDC_IDENTITY_PROVIDER_NAME: ""
+OIDC_IDENTITY_PROVIDER_SCOPES: ""
+OIDC_IDENTITY_PROVIDER_USERNAME_CLAIM: ""
+OS_ARCH: amd64
+OS_NAME: ubuntu
+OS_VERSION: "20.04"
+SERVICE_CIDR: 100.64.0.0/13
+TKG_HTTP_PROXY_ENABLED: "false"
+EOF
+
+######## install Tanzu CLI/Tools ########
+
 apt install unzip
 curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
 unzip awscliv2.zip
@@ -69,4 +132,9 @@ apt install docker-ce -y
 
 tanzu management-cluster permissions aws set
 
+sleep 10 
+
 #tanzu management-cluster create -b 192.168.1.76:80 --ui 6
+
+
+tanzu management-cluster create --file mgmt.yaml -v 6
